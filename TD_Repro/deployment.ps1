a8dec310c9d36789f347ae59918655f6a2a5d426
Azure Aaron <aarosanders@microsoft.com>
2023-11-21 22:01:34 -0600

    built durr

--- TD_Repro/deployment.ps1
+++ TD_Repro/deployment.ps1
@@ -1,7 +1,7 @@
 # This file will be used for testing purposes until a proper CI/CD pipeline is in place.
 
-$mainBicepFile = ".\TD_Repro\src\main.bicep"
-$mainJSONFile = ".\TD_Repro\src\main.json"
+$mainBicepFile = ".\src\main.bicep"
+$mainJSONFile = ".\src\main.json"
 #$mainParameterFile = ".\virtualMachines.parameters.json"
 
 $start = get-date -UFormat "%s"
@@ -13,37 +13,37 @@ Write-Host "`nBuilding main.json from main.bicep.."
 bicep build $mainBicepFile --outfile $mainJSONFile
 
 
-# Specifies the account and subscription where the deployment will take place.
-if (!$subID) {
-    $subID = Read-Host "Please enter the Subscription ID that you want to deploy this Resource Group to: "
-}
-#Set-AzContext -Subscription $subID
+# # Specifies the account and subscription where the deployment will take place.
+# if (!$subID) {
+#     $subID = Read-Host "Please enter the Subscription ID that you want to deploy this Resource Group to: "
+# }
+# #Set-AzContext -Subscription $subID
 
-$iteration = "0012"
-$scenario_Name = "privatelink${iteration}"
-$rgName = "Connection_${scenario_Name}_Sandbox"
-$locationClient = 'westeurope'
-$locationServer = 'westeurope'
-$randomFiveLetterString = .\scripts\deployment_Scripts\Get-LetterGUID.ps1
+# $iteration = "0012"
+# $scenario_Name = "privatelink${iteration}"
+# $rgName = "Connection_${scenario_Name}_Sandbox"
+# $locationClient = 'westeurope'
+# $locationServer = 'westeurope'
+# $randomFiveLetterString = .\scripts\deployment_Scripts\Get-LetterGUID.ps1
 
-# Might have to test with the same size VM the customer uses.
-# --Update 11/20 - Could not repro with same size as customer's VM
-# $virtualMachine_Size = 'Standard_E48s_v5'
-$virtualMachine_Size = 'Standard_E4d_v5'
+# # Might have to test with the same size VM the customer uses.
+# # --Update 11/20 - Could not repro with same size as customer's VM
+# # $virtualMachine_Size = 'Standard_E48s_v5'
+# $virtualMachine_Size = 'Standard_E4d_v5'
 
-Write-Host "Creating ${rgName}"
-New-AzResourceGroup -Name $rgName -Location $locationClient
+# Write-Host "Creating ${rgName}"
+# New-AzResourceGroup -Name $rgName -Location $locationClient
 
-Write-Host "`nStarting Bicep Deployment.."
-New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $mainBicepFile -TemplateParameterFile $mainParameterFile `
-    -locationClient $locationClient -locationServer $locationServer `
-    -virtualMachine_Size $virtualMachine_Size `
-    -storageAccount_Name "plconntestsa${randomFiveLetterString}" `
-    ##-scenario_Name $scenario_Name `
-    -numberOfClientVMs 1 `
-    -numberOfServerVMs 1
-# -usingAzureFirewall $false
-# -storageAccount_ID $storageAccount_ID `
+# Write-Host "`nStarting Bicep Deployment.."
+# New-AzResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile $mainBicepFile -TemplateParameterFile $mainParameterFile `
+#     -locationClient $locationClient -locationServer $locationServer `
+#     -virtualMachine_Size $virtualMachine_Size `
+#     -storageAccount_Name "plconntestsa${randomFiveLetterString}" `
+#     ##-scenario_Name $scenario_Name `
+#     -numberOfClientVMs 1 `
+#     -numberOfServerVMs 1
+# # -usingAzureFirewall $false
+# # -storageAccount_ID $storageAccount_ID `
 
 $end = get-date -UFormat "%s"
 $timeTotalSeconds = $end - $start
