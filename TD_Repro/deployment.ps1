6678e940ac3a7275967f93fb6c92747e51e2a4cf
Azure Aaron <aarosanders@microsoft.com>
2023-11-21 18:39:55 -0600

    trypo

--- TD_Repro/deployment.ps1
+++ TD_Repro/deployment.ps1
@@ -17,7 +17,7 @@ bicep build $mainBicepFile --outfile $mainJSONFile
 if (!$subID) {
     $subID = Read-Host "Please enter the Subscription ID that you want to deploy this Resource Group to: "
 }
-Set-AzContext -Subscription $subID
+#Set-AzContext -Subscription $subID
 
 $iteration = "0012"
 $scenario_Name = "privatelink${iteration}"
