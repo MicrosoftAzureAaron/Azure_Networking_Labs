5666259063417a988304bae7612ec6a192d5a745
jimgodden <james.c.godden@outlook.com>
2023-11-30 11:37:32 -0500

    fixing choco installs

--- scripts/WinServ2022_WebServer_InitScript.ps1
+++ scripts/WinServ2022_WebServer_InitScript.ps1
@@ -2,19 +2,19 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install PowerShell Core
-Start-Job -ScriptBlock { choco install powershell-core }
+Start-Job -ScriptBlock { choco install powershell-core -y }
 
 # Install Python 3.11
-Start-Job -ScriptBlock { choco install python311 }
+Start-Job -ScriptBlock { choco install python311 -y }
 
 # Install Visual Studio Code
-Start-Job -ScriptBlock { choco install vscode }
+Start-Job -ScriptBlock { choco install vscode -y }
 
 # Install Wireshark
-Start-Job -ScriptBlock { choco install wireshark }
+Start-Job -ScriptBlock { choco install wireshark -y }
 
 # Install PsTools
-Start-Job -ScriptBlock { choco install pstools }
+Start-Job -ScriptBlock { choco install pstools -y }
 
 # Wait for all jobs to finish
 Get-Job | Wait-Job
